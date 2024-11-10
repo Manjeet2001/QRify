59269861e00343c02ea3b445b270e4dfbfeef2ef
Manjeet2001 <manjeetydv16@gmail.com>
2024-11-10 18:00:53 +0530

    added Zxing core and Zxing Java Extension dependecies

---
+++ QR_Code_Generator/src/main/java/com/QR_generator/QR_Code_Generator/QRCodeGenerator.java
@@ -0,0 +1,7 @@
+package com.QR_generator.QR_Code_Generator;
+
+public class QRCodeGenerator {
+    public static void main(String[] args){
+
+    }
+}
---
+++ QR_Code_Generator/src/test/java/com/QR_generator/QR_Code_Generator/QrCodeGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.QR_generator.QR_Code_Generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrCodeGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
